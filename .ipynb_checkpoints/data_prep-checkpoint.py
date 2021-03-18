0d2f3a43245a7856ede53ed01e97ddd96679d117
Konstantinos Tsakonas <iamtsac@gmail.com>
2021-03-18 21:25:42 +0200

    cen,norm,stand done

--- .ipynb_checkpoints/data_prep-checkpoint.py
+++ .ipynb_checkpoints/data_prep-checkpoint.py
@@ -19,17 +19,9 @@ y_test = test_csv['label'].to_numpy()
 x_train=x_train.reshape(60000,28,28)
 x_test=x_test.reshape(10000,28,28) 
 
-
-x_centralized = np.zeros((60000,28,28))
-x_normalized = np.zeros((60000,28,28)) 
-x_standardized = np.zeros((60000,28,28))
-
+# NORMALIZATION METHOD
+x_normalized = np.zeros((60000,28,28))
 for i in range(0,60000):
-    # CENTRALIZATION
-    scaler = preprocessing.StandardScaler(with_std=False).fit(x_train[i])
-    x_centralized[i] = scaler.transform(x_train[i]) 
-    # NORMALIZATION
-    x_normalized[i]=preprocessing.normalize(x_train[i],norm='l2') 
-    # STANDARDIZATION
-    scaler = preprocessing.StandardScaler(with_mean=False).fit(x_train[i])
-    x_standardized[i] = scaler.transform(x_train[i])
+    x_normalized[i]=normalize(x_train[i],norm='l2')
+
+print(np.shape(x_normalized))
