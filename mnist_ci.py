16fb81d27e18e2ddfc15835918b06c2e0c959274
Konstantinos Tsakonas <iamtsac@gmail.com>
2021-04-16 05:16:54 +0300

    polish buld function

--- mnist_ci.py
+++ mnist_ci.py
@@ -1,11 +1,13 @@
 import sys
 import pandas as pd
 import numpy as np
+from numpy import std
+from numpy import mean 
 import tensorflow as tf
-from scipy.io import savemat
-from matplotlib import pyplot as plt
+from statistics import mean
 from sklearn import preprocessing
 from sklearn.model_selection import KFold 
+from matplotlib import pyplot 
 
 #------------------------------------------------# 
 
@@ -61,7 +63,7 @@ def data_preprocessing(x_train, y_train, x_test, y_test, preprocessing_type='nor
         x_test_standardize =  np.zeros((np.shape(x_test)[0], 28, 28)) 
 
         for i,j in zip(range(np.shape(x_train)[0]),range(np.shape(x_test)[0])): 
-
+        
             scaler = preprocessing.StandardScaler(with_mean=False).fit(x_train[i])
             scaler_test = preprocessing.StandardScaler(with_std=False).fit(x_train[j])
             x_standardize[i] = scaler.transform(x_train[i])
@@ -70,47 +72,77 @@ def data_preprocessing(x_train, y_train, x_test, y_test, preprocessing_type='nor
 
         return x_standardize, y_train, x_test_standardize, y_test
 
-def build_model(hidden_nodes=397,n=0.001,m=0,loss='categorical_crossentropy',metric=['accuracy']):
+def summarize_diagnostics(histories):
+    for i in range(len(histories)):
+        # plot loss
+        pyplot.subplot(2, 1, 1)
+        pyplot.title('Cross Entropy Loss')
+        pyplot.plot(histories[i].history['loss'], color='blue', label='train')
+        pyplot.plot(histories[i].history['val_loss'], color='orange', label='test')
+        # plot accuracy
+        pyplot.subplot(2, 1, 2)
+        pyplot.title('Classification Accuracy')
+        pyplot.plot(histories[i].history['accuracy'], color='blue', label='train')
+        pyplot.plot(histories[i].history['val_accuracy'], color='orange', label='test')
+    pyplot.show()
 
-    input_shape=(28,28)
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Flatten()) # Change input from shape (,28,28) to (,784)
-    model.add(tf.keras.layers.Dense(512, activation='relu'))
-    model.add(tf.keras.layers.Dense(10, activation='softmax'))
-    
-    model.compile(
-        optimizer=tf.keras.optimizers.SGD(learning_rate=n,momentum=m),
-        loss=loss,
-        metrics=metric
-    )
-    return model
 
-def train_model(x_train,y_train,x_test,y_test,epochs=5):
+def summarize_performance(scores):
+    # print summary
+    print('Accuracy: mean=%.3f std=%.3f, n=%d' % (mean(scores)*100, std(scores)*100, len(scores)))
+    # box and whisker plots of results
+    pyplot.boxplot(scores)
+    pyplot.show()
+
+def train_model(x_train,y_train,x_test,y_test,epochs=5,nodes=10,verbose=0,loss='categorical_crossentropy',metric=['accuracy'],learning_rate=0.001,momentum=0,plot='off'):
 
     fold_number = 0
+    scores ,histories = list(), list()
     sum_of_loss = sum_of_acc =  0
     kfold = KFold(n_splits=5, shuffle=False)
+
+
+    # K-Fold iterations
     for train_index, test_index in kfold.split(x_train,y_train):  
 
-        model = build_model(128) 
-        model.fit(
+        # Model configuration
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Flatten()) # Change input from shape (,28,28) to (,784)
+        model.add(tf.keras.layers.Dense(nodes, activation='relu'))
+        model.add(tf.keras.layers.Dense(10, activation='softmax'))
+        
+        model.compile(
+            optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate,momentum=momentum),
+            loss=loss,
+            metrics=metric
+        )
+
+        history = model.fit(
             x_train[train_index],
             y_train[train_index],
-            epochs=15,
-            verbose=0,
+            epochs=epochs,
+            verbose=verbose,
+            validation_data=(x_train[test_index], y_train[test_index])
             ) 
 
         val_loss, val_acc = model.evaluate(x_train[test_index], y_train[test_index],verbose=0)
+
+
         print('|----------------------------------------------------------------------------|')
         fold_number += 1
         print("|  For fold ",(fold_number),"\n|  Loss: ", val_loss, " Accuracy: ",val_acc)
         sum_of_acc += val_acc
-        sum_of_loss += val_loss
+        sum_of_loss += val_loss 
+        scores.append(val_acc)
+        histories.append(history)
     print('|----------------------------------------------------------------------------|')
-    print("\n \n The average of the Loss and Accuracy is: \n", "Loss: ",sum_of_loss/fold_number,"\n","Accuracy: ",sum_of_acc/fold_number," \n ")
-    
+    print("\n \n The average of the Loss and Accuracy is: \n", "Loss: ",sum_of_loss/fold_number,"\n","Accuracy: ",sum_of_acc/fold_number," \n ") 
+    if(plot == 'on'):
+        summarize_diagnostics(histories)
+        # summarize estimated performance
+        summarize_performance(scores)
 
-    return model, val_acc, val_loss
+    return model 
 
 
 def prediction(model,x_test,y_test):
@@ -125,15 +157,24 @@ def init_data(train_csv, test_csv,preprocessing_type='normalization'):
 
     return  data_preprocessing(x_train,y_train, x_test,y_test,preprocessing_type)
 
-def init_training(learning_rate=1,loss='sparse_categorical_crossentropy',metric=['accuracy']):
 
-    x_train, y_train, x_test, y_test = init_data('data/mnist_train.csv','data/mnist_test.csv') 
-    np.set_printoptions(threshold=sys.maxsize)
-    print(x_train[0])
-    model,val_acc,val_loss = train_model(x_train,y_train, x_test,y_test )
-    prediction(model, x_test, y_test)
+x_train, y_train, x_test, y_test = init_data('data/mnist_train.csv','data/mnist_test.csv') 
+#model = train_model(x_train,y_train,x_test,y_test,nodes=397,epochs=20,verbose=1,learning_rate=0.001,plot='on')
 
-init_training()
+hidden_nodes = [10,397,794]
+loss_metrics = [ 'categorical_crossentropy','mse']
+
+for node in hidden_nodes:
+    for loss in loss_metrics:
+        print("|----------------------------------------------------", "\n|  LOSS METRIC IS: ",loss," ")
+        print('|----------------------------------------------------','\n|  NUM OF NODES: ', node," ")
+        print('|----------------------------------------------------')
+
+        model = train_model(x_train,y_train,x_test,y_test,nodes=node,epochs=20,verbose=0,learning_rate=0.001,plot='off',loss=loss)
+        
+
+#prediction(model, x_test, y_test)
+
 
 
 #             ----------------------------------- Probably to be used -------------------------------------------------
@@ -142,6 +183,14 @@ init_training()
 #             ---     if np.array_equal(classes,y_test):                                                            ---
 #             ---         print("predictions are equal with the expected")                                          ---
 #             ---     else:                                                                                         ---
-#             ---         print("they are not equal, there were some false predictions")                            ---
+#             ---         print("they are not equal, there were some false predictions")                            --- 
+#             ---        print(mean( history.history['accuracy'] ))                                                 ---
+#             ---        plt.plot(history.history['loss'])                                                          ---
+#             ---        plt.plot(history.history['val_loss'])                                                      ---
+#             ---        plt.title('model loss'                                                                     ---
+#             ---        plt.ylabel('loss')                                                                         ---
+#             ---        plt.xlabel('epoch')                                                                        ---
+#             ---        plt.legend(['train', 'test'], loc='upper left'                                             ---
+#             ---        plt.show()                                                                                 ---
 #             ---------------------------------------------------------------------------------------------------------
 #             ---------------------------------------------------------------------------------------------------------
